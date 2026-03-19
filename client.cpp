a2b20e05d9f2aeb97fb4eefa32e5de88400ae839
seandang38 <seandangdev@gmail.com>
2026-03-19 16:25:43 -0500

    updated

---
+++ client.cpp
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <strings.h>
+
+int main() {
+
+
+
+
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<strings.h>
+#include<netdb.h>
+
+int main() {
+
+
+
+
+    return 0;
+}
